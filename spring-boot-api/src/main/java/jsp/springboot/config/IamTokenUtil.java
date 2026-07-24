2f0d6fbedd95ceb6db7fe1cb1154b200abbb4852
Aravindhan A <2000199101@hexaware.com>
2026-07-24 13:12:35 +0530

    latest config and application token auto refresh

---
+++ spring-boot-api/src/main/java/jsp/springboot/config/IamTokenUtil.java
@@ -0,0 +1,21 @@
+package jsp.springboot.config;
+
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rds.RdsUtilities;
+
+@Component
+public class IamTokenUtil {
+
+    public String generateToken() {
+        RdsUtilities utilities = RdsUtilities.builder()
+                .region(Region.US_EAST_1)
+                .build();
+
+        return utilities.generateAuthenticationToken(r -> r
+                .hostname("publicip.dsql.us-east-1.on.aws")
+                .port(5432)
+                .username("admin")
+        );
+    }
+}
