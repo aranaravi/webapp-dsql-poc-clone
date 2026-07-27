8cddec66853639a70ae4810d3de41e1c2db45cde
Aravindhan Alagesan <aravindh2090@gmail.com>
2026-07-27 19:36:37 +0530

    Update Keycloak URL in configuration

--- angular-app/src/app/auth/keycloak.config.ts
+++ angular-app/src/app/auth/keycloak.config.ts
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new (Keycloak as any)({
-  url: 'https://44.227.63.113',
+  url: 'https://login.amplify.com',
   realm: 'amplify',
   clientId: 'dsql-poc-client'
 });
